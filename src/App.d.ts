b21a121a1a76542e7fc2606facd48bb1ae105334
rin <sayuri.shindo@gmail.com>
2025-11-19 16:40:06 +0900

    本番ビルド結果

---
+++ src/App.d.ts
@@ -0,0 +1,3 @@
+import './scss/App.scss';
+declare function App(): import("react/jsx-runtime").JSX.Element;
+export default App;
---
+++ src/components/NavigationBar.d.ts
@@ -0,0 +1,3 @@
+import '../scss/NavigationBar.scss';
+declare function NavigationBar(): import("react/jsx-runtime").JSX.Element;
+export default NavigationBar;
